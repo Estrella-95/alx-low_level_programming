722168162dba3e690ee51ecb0bdf29e78a4909f5
Estrella-95 <estelle.naynay@gmail.com>
2022-03-15 10:35:18 -0700

    print_alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet x10 . repeat 10 times
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	int 1; 
+	char c;
+
+	for (1 = 0; 1 < 10; 1++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n')
+
+	}
+}
