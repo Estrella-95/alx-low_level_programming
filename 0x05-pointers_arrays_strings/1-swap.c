d8a930642ee503fa4d2a634b69f893d20a6a24cb
Estrella-95 <estelle.naynay@gmail.com>
2022-03-22 11:07:48 -0700

    swap values of an integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * swap_int - swap the values of two integers a and b
+ * swap_int - swap the valuses of two integers a and b
+ *
  * @a: A pointer to an int that will be updated
- * #b: B pointer to an int that will be updated
- * Return:Void that means our answer is correct
+ *
+ * @b: B pointer to an int that will be updated
+ *
+ * Return: void that means our answer is correct
+ *
  */
 
 void swap_int(int *a, int *b)
+
 {
 	int f;
 
@@ -17,4 +22,3 @@ void swap_int(int *a, int *b)
 
 	*b = f;
 }
-
